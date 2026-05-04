a22a1d0ae8c41bef41ae6bbbd6ef748d0f079da7
raphbochud-svg <raphbochud@gmail.com>
2026-05-04 09:42:01 +0200

    au revoir test

--- weather.py
+++ weather.py
@@ -115,5 +115,3 @@ def get_weather(lat: float, lon: float, num_days: int) -> list[dict]:
 if __name__ == "__main__":
     for day in get_weather(lat=47.37, lon=8.54, num_days=3):
         print(day)
-
-test
